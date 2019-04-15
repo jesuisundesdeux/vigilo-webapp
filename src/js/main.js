dcf3d7cdb3d72e261efefb70e43cc5b0ff3d59c7
tcoupin <thibault.coupin@gmail.com>
2019-04-15 23:11:59 +0200

    Navbar, card grid, and map

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,15 @@ import '../index.html';
 import '../css/main.scss';
 
 import $ from 'jquery';
-//window.$ = window.jQuery = global.$ = global.jQuery = $;
 import M from 'materialize-css';
+import L from 'leaflet';
+// Fix import leaflet with webpack https://github.com/PaulLeCam/react-leaflet/issues/255
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 
 import * as vigilo from './vigilo';
@@ -13,19 +20,63 @@ import * as vigiloui from './vigilo-ui';
 
 M.AutoInit();
 
-
-// Issues list
+/**
+ * Functions for issues list
+ */
 let offset=0;
-function printIssues(count){
-	vigilo.getIssues().then((issues) => {
-		issues = issues.slice(offset, offset+count);
-		console.log(issues)
-		offset+=issues.length;
-		issues.forEach((issue)=>{
-			$(".container.issues .cards-container").append(vigiloui.issueCard(issue))
-		})
-	})
-	
+async function displayIssues(count){
+	var issues = await vigilo.getIssues()
+	issues = issues.slice(offset, offset+count);
+	console.log(issues)
+	offset+=issues.length;
+	issues.forEach((issue)=>{
+		$(".issues .cards-container").append(vigiloui.issueCard(issue))
+	})	
 }
-printIssues(10)
-$('.more-issues a').click(()=>{printIssues(10)})
+displayIssues(10)
+$('.more-issues a').click(()=>{displayIssues(10)})
+
+async function viewIssue(token){
+	var modal = M.Modal.getInstance($("#modal-issue")[0]);
+	var issues = await vigilo.getIssues();
+	var issue = issues.filter(item => item.token == token)[0];
+	$("#modal-issue").empty().append(vigiloui.issueDetail(issue));
+	modal.open()
+}
+window.viewIssue = viewIssue
+
+/**
+ * Functions for issues map
+ */
+var issuesmap;
+async function initMap(){
+	if (issuesmap !== undefined){
+		issuesmap.invalidateSize()
+		return;
+	}
+	var issues = await vigilo.getIssues();
+	$('#issues-map').empty();
+	issuesmap = L.map('issues-map').setView([43.605413, 3.879568], 11);
+
+	L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
+		attribution: 'copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>',
+		subdomains: 'abcd',
+		minZoom: 0,
+		maxZoom: 20,
+		ext: 'png'
+	}).addTo(issuesmap);
+
+	for (var i in issues){
+		L.marker([issues[i].lat_float, issues[i].lon_float]).addTo(issuesmap);
+	}
+}
+M.Tabs.getInstance($("nav .tabs")[0]).options.onShow = function(){initMap()}
+
+async function centerOnIssue(token){
+	await initMap()
+	var issues = await vigilo.getIssues();
+	var issue = issues.filter(item => item.token == token)[0];
+	issuesmap.setView([issue.lat_float, issue.lon_float],19)
+	M.Tabs.getInstance($("nav .tabs")[0]).select('issues-map')
+}
+window.centerOnIssue = centerOnIssue
--- src/js/vigilo-ui.js
+++ src/js/vigilo-ui.js
@@ -1,22 +1,29 @@
 export function issueCard(issue){
     return `
-    <div class="col s12 m6 l4">
-        <div class="card">
-            <!--div class="card-image">
+    <div class="col s12 m12 l6 xl4">
+        <div class="card horizontal">
+            <div class="card-image">
                 <img src="${issue.img}">
-            </div-->
-            <!--div class="card-stacked"-->
+            </div>
+            <div class="card-stacked">
                 <div class="card-content">
                     <span class="card-title"></span>
-                    <p>${issue.token}</p>
-                    <p>${issue.comment}</p>
-                    <p>${issue.date_obj.toLocaleString()} - ${issue.address}</p>
+                    <h5>${issue.categorie_str}</h5>
+                    <p class="grey-text">${issue.address}</p>
+                    <p class="grey-text">${issue.date_obj.toLocaleString()}</p>
                 </div>
-                <div class="card-action">
-                    <a href="#">This is a link</a>
+                <div class="card-action right-align">
+                    <a class="waves-effect waves-light btn-floating" onclick="viewIssue('${issue.token}')"><i class="material-icons center">search</i></a>
+                    <a class="waves-effect waves-light btn-floating" onclick="centerOnIssue('${issue.token}')"><i class="material-icons center">map</i></a>
                 </div>
-            <!--/div-->
+            </div>
         </div>
     </div>
+  `
+}
+
+export function issueDetail(issue){
+    return `
+    <h2>${issue.token}</h2>
   `
 }
