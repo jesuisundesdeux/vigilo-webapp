7687cb9d0e81788ee2e7485df5c5709780957621
tcoupin <thibault.coupin@gmail.com>
2019-05-09 22:52:35 +0200

    Use citylist.json, allow form on every server

--- src/js/vigilo-config.js
+++ src/js/vigilo-config.js
@@ -1,20 +1,25 @@
-export const INSTANCES = [
-    {
-        name: "Montpellier",
-        scope: "34_montpellier",
-        url: "https://vigilo.jesuisundesdeux.org",
-    },
-    {
-        name: "Châtillon",
-        scope: "92_chatillon",
-        url: "https://api.vigilo.vpchatillon.fr"
-    },
-    {
-        name: "Dev-vigilo",
+import {request} from './utils';
+
+const SCOPES_URL="https://vigilo-bf7f2.firebaseio.com/citylist.json";
+
+export async function getInstances(){
+    
+    var scopes = await request(SCOPES_URL);
+    scopes["Developpement"] = {
+        prod: false,
         scope: "develop",
-        url: "https://dev-vigilo.alwaysdata.net",
-    },
-]
+        api_path: "https://dev-vigilo.alwaysdata.net"
+    }
+    scopes = Object.entries(scopes).map((item)=>{
+        item[1].name = item[0];
+        return item[1]
+    })
+    if (window.location.search.indexOf('beta') == -1){
+        scopes = scopes.filter((item)=>item.prod)
+    }
+    return scopes
+
+} 
 
 var pkg= require('../../package.json');
 export const VERSION = pkg.name+"-"+pkg.version
@@ -28,10 +33,11 @@ export function getInstance(){
         return JSON.parse(instance)
     }
 }
-function setInstance(name){
-    for (var i in INSTANCES){
-        if (INSTANCES[i].name == name){
-            localStorage.setItem('vigilo-instance', JSON.stringify(INSTANCES[i]))
+async function setInstance(name){
+    var instances = await getInstances()
+    for (var i in instances){
+        if (instances[i].name == name){
+            localStorage.setItem('vigilo-instance', JSON.stringify(instances[i]))
             break;
         }
     }
