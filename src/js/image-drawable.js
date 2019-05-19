abd4018a15f1b329e91129df9a32080035ed99e5
tcoupin <thibault.coupin@gmail.com>
2019-05-19 15:19:54 +0200

    Daw on image work on computer

--- src/js/image-drawable.js
+++ src/js/image-drawable.js
@@ -1,11 +1,19 @@
 import $ from 'jquery';
 import M from 'materialize-css';
 
+
 export class ClassImageDrawable {
+
     constructor(div) {
+        this.queue_length = 10;
+        this.backHistory = [];
+        this.upHistory = [];
+        this.drawing = false;
+
         this.div = div;
         this.ctx = $(div).find('canvas')[0].getContext('2d');
-        this.color = "";
+        this.height = $(div).find('canvas')[0].height;
+        this.width = $(div).find('canvas')[0].width;
         this.initBtn();
         this.setColor('yellow');
         $(this.div).addClass('drawable');
@@ -41,13 +49,15 @@ export class ClassImageDrawable {
         $(this.div).find("ul li a").click(function(){
           self.setColor($(this).data('color'))
         })
-
-
+        $(this.div).find("a:contains('undo')").click(this.undo.bind(this))
+        $(this.div).find("a:contains('redo')").click(this.redo.bind(this))
+        this.updateBtnStatus()
     }
 
     setColor(newColor){
       $(this.div).find('#colorpicker').removeClass(this.color).addClass(newColor);
       this.color=newColor;
+      this.ctx.fillStyle = this.color;
     }
 
     isFullscreen() {
@@ -58,12 +68,109 @@ export class ClassImageDrawable {
         $(this.div).addClass('fullscreen');
         e.stopPropagation();
         e.preventDefault();
+
+        $(this.div).find('canvas').on('mousedown touchstart', this.startDraw.bind(this))
+        $(this.div).find('canvas').on('mouseup mouseleave touchend touchleave', this.stopDraw.bind(this))
+        $(this.div).find('canvas').on('mousemove touchmove', this.onDraw.bind(this))
     }
 
     close(e) {
         $(this.div).removeClass('fullscreen');
         e.stopPropagation();
         e.preventDefault();
+
+        this.stopDraw();
+
+        $(this.div).find('canvas').off('mousedown touchstart');
+        $(this.div).find('canvas').off('mouseup mouseleave touchend touchleave');
+        $(this.div).find('canvas').off('mousemove touchmove');
+    }
+
+    updateBtnStatus() {
+      if (this.backHistory.length == 0){
+        $(this.div).find("a:contains('undo')").addClass('disabled')
+      } else {
+        $(this.div).find("a:contains('undo')").removeClass('disabled')
+      }
+      if (this.upHistory.length == 0){
+        $(this.div).find("a:contains('redo')").addClass('disabled')
+      } else {
+        $(this.div).find("a:contains('redo')").removeClass('disabled')
+      }
+    }
+
+    saveImage() {
+      // push image to history and clean upcomming history
+      var data = this.ctx.getImageData(0,0,this.width,this.height);
+      this.backHistory.push(data);
+      this.backHistory.slice(-this.queue_length);
+      this.upHistory = []
+      this.updateBtnStatus()
+    }
+
+    redo() {
+      if (this.upHistory.length == 0){
+        return
+      }
+      // push image to back history and shift up history to canvas
+      var data = this.ctx.getImageData(0,0,this.width,this.height);
+      this.backHistory.push(data);
+      data = this.upHistory.shift(this.backHistory);
+      this.ctx.putImageData(data,0,0,0,0,this.width,this.height);
+
+      this.updateBtnStatus()
+    }
+
+    undo() {
+      if (this.backHistory.length == 0){
+        return
+      }
+      // unshift image to upcomming history and pop back history to canvas
+      var data = this.ctx.getImageData(0,0,this.width,this.height);
+      this.upHistory.unshift(data);
+      data = this.backHistory.pop(this.backHistory);
+      this.ctx.putImageData(data,0,0,0,0,this.width,this.height);
+
+      this.updateBtnStatus()
+    }
+
+    startDraw(e){
+      if (!this.drawing){
+        this.drawing = true;
+        this.ratioWidth = this.width / $("canvas").last().width();
+        this.ratioHeight = this.height/$("canvas").last().height();
+        this.saveImage();
+        console.log('start')
+        e.stopPropagation();
+        e.preventDefault();
+      }
+    }
+
+    stopDraw(e){
+      if (this.drawing){
+        this.drawing = false
+        console.log('stop')
+        e.stopPropagation();
+        e.preventDefault();
+      }
+    }
+
+    onDraw(e) {
+      if (!this.drawing){
+        return;
+      }
+      var x,y;
+      if (e.type == 'touchmove'){
+        var rect = e.target.getBoundingClientRect();
+        x = e.changedTouches[0].pageX * this.ratioWidth - rect.left;
+        y = e.changedTouches[0].pageY * this.ratioHeight - rect.top;
+      } else {
+        x = e.offsetX * this.ratioWidth;
+        y = e.offsetY * this.ratioHeight
+      }
+      this.ctx.fillRect(x,y,10*this.ratioWidth, 10*this.ratioHeight);
+      e.stopPropagation();
+      e.preventDefault();
     }
 }
 
