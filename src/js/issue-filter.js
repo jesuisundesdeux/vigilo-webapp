c3b21880d2c88a8124af053d4cffdbef523e8ae6
tcoupin <thibault.coupin@gmail.com>
2019-05-27 21:48:36 +0200

    filter: global checkbox

--- src/js/issue-filter.js
+++ src/js/issue-filter.js
@@ -23,6 +23,28 @@ import dataManager from './dataManager';
 				hour: $.map($("#modal-filters input[name=hour]:checked"), (i)=>$(i).val())
 			})
 		}
+
+		function updateCheckbox(){
+			if ($(this).parent().parent().find('input:checked').length == 0){
+				$(this).parent().parent().find('h6 i').html('check_box_outline_blank');
+			} else if ($(this).parent().parent().find('input:checked').length != $(this).parent().parent().find('input').length){
+				$(this).parent().parent().find('h6 i').html('indeterminate_check_box');
+			} else {
+				$(this).parent().parent().find('h6 i').html('check_box');
+			}
+		}
+
+		$('#modal-filters h6').click(function(){
+			if ($(this).find('i').html() == "check_box"){
+				$(this).parent().find('input').prop('checked',false)
+			} else {
+				$(this).parent().find('input').prop('checked',true)
+			}
+			updateCheckbox.call($(this).parent().find('label').first())
+		});
+
+		$('#modal-filters label').click(updateCheckbox);
+		
 		
     } catch (e) {
         $("#issues .cards-container").empty().append(vigiloui.errorCard(e));
