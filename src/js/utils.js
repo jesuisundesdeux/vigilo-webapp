f1aa3bafaae089a926d8dde0e6c22ab03b693aa9
Thibault Coupin <thibault.coupin@gmail.com>
2019-11-21 22:03:11 +0100

    Optimisation du cache ajax

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,73 @@
 const CONTENT_TYPE_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded"
+const STATUS_PENDING = "pending";
+const STATUS_OK = "OK";
+const STATUS_KO = "KO";
 let requests_cache = {};
+
+class Deferred {
+    constructor() {
+        this.promise = new Promise((resolve, reject) => {
+            this.reject = reject
+            this.resolve = resolve
+        })
+    }
+}
+
+function make_request(options) {
+
+    requests_cache[options.url] = {
+        status: STATUS_PENDING,
+        listeners: []
+    };
+
+
+    return (new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(options.method, options.url);
+        for (var i in options.headers) {
+            xhr.setRequestHeader(i, options.headers[i]);
+        }
+        xhr.onload = () => resolve(xhr);
+        xhr.onerror = () => {
+            requests_cache[options.url].status = STATUS_KO;
+            for (var i in requests_cache[options.url].listeners) {
+                requests_cache[options.url].listeners[i].reject(`HTTP Code: ${xhr.status} (${xhr.statusText})\n${xhr.responseText}`);
+            }
+            reject(`HTTP Code: ${xhr.status} (${xhr.statusText})\n${xhr.responseText}`);
+        }
+        xhr.send(options.body);
+    })).then((xhr) => {
+        if (xhr.status != 200) {
+            requests_cache[options.url].status = STATUS_KO;
+            for (var i in requests_cache[options.url].listeners) {
+                requests_cache[options.url].listeners[i].reject();
+            }
+            return Promise.reject()
+        }
+        try {
+            var response = JSON.parse(xhr.responseText)
+            if (response.status !== undefined && response.status != 0) {
+                throw new Error('error parsing json');
+            }
+            requests_cache[options.url].status = STATUS_OK;
+            requests_cache[options.url].response = response;
+            for (var i in requests_cache[options.url].listeners) {
+                requests_cache[options.url].listeners[i].resolve(response);
+            }
+            return Promise.resolve(response)
+        } catch (e) {
+            requests_cache[options.url].status = STATUS_KO;
+            for (var i in requests_cache[options.url].listeners) {
+                requests_cache[options.url].listeners[i].reject(`HTTP Code: ${xhr.status} (${xhr.statusText})\n${xhr.responseText}`);
+            }
+            return Promise.reject(`HTTP Code: ${xhr.status} (${xhr.statusText})\n${xhr.responseText}`)
+        }
+    });
+}
+
 export function request(options, nocache) {
     nocache = nocache || false;
+
     if (typeof options == "string") {
         options = { url: options }
     }
@@ -12,33 +78,17 @@ export function request(options, nocache) {
         options.body = Object.entries(options.body).map(x => x[0] + "=" + encodeURIComponent(x[1])).join("&")
     }
 
+
     if (!nocache && options.method == "GET" && requests_cache[options.url] !== undefined) {
-        return Promise.resolve(requests_cache[options.url]);
+        if (requests_cache[options.url].status == STATUS_OK) {
+            return Promise.resolve(requests_cache[options.url].response);
+        } else if (requests_cache[options.url].status == STATUS_PENDING) {
+            var def = new Deferred();
+            requests_cache[options.url].listeners.push(def);
+            return def.promise;
+        }
     } else {
-        return (new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.open(options.method, options.url);
-            for (var i in options.headers) {
-                xhr.setRequestHeader(i, options.headers[i]);
-            }
-            xhr.onload = () => resolve(xhr);
-            xhr.onerror = () => reject(`HTTP Code: ${xhr.status} (${xhr.statusText})\n${xhr.responseText}`);
-            xhr.send(options.body);
-        })).then((xhr) => {
-            if (xhr.status != 200) {
-                return Promise.reject()
-            }
-            try {
-                var response = JSON.parse(xhr.responseText)
-                if (response.status !== undefined && response.status != 0) {
-                    throw new Error('error parsing json');
-                }
-                requests_cache[options.url] = response;
-                return Promise.resolve(response)
-            } catch (e) {
-                return Promise.reject(`HTTP Code: ${xhr.status} (${xhr.statusText})\n${xhr.responseText}`)
-            }
-        });
+        return make_request(options)
     }
 
 };
